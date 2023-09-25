1ecb792097e0f4f041c08bf958428f5fb19ec05c
Daria Koshubarova <koshubarova.da@gmail.com>
2023-09-25 22:06:47 +0600

    ПР13

--- app.js
+++ app.js
@@ -25,6 +25,10 @@ app.use((req, res, next) => {
 app.use('/', require('./routes/users'));
 app.use('/', require('./routes/cards'));
 
+app.use('/*', (req, res) => {
+  res.status(404).send({ message: 'Страница не найдена' });
+});
+
 app.listen(PORT, () => {
   console.log('Listening 3000');
 });
