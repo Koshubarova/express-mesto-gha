b2b693eb9a9f02d84217708203351d535cc17296
Daria Koshubarova <koshubarova.da@gmail.com>
2023-09-25 21:42:28 +0600

    ПР13

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '650eef36cae69ada701e0c50'
+  };
+
+  next();
+});
+
+app.use('/', require('./routes/users'));
+app.use('/', require('./routes/cards'));
+
+app.listen(PORT, () => {
+  console.log('Listening 3000');
+});
---
+++ routes/users.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+const { getUsers, getUserById, addUser, editUser, editUserAvatar } = require('../controllers/users');
+
+router.get('/users', getUsers);
+
+router.get('/users/:userId', getUserById);
+
+router.post('/users', addUser);
+
+router.patch('/users/me', editUser);
+
+router.patch('/users/me/avatar', editUserAvatar);
+
+module.exports = router;
