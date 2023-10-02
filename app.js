9dec6633346905fb37d90277d4cd865e057e9bfc
Daria Koshubarova <koshubarova.da@gmail.com>
2023-10-02 17:52:58 +0600

    14.4

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ const { addUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 
 const app = express();
-app.use(errors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -51,7 +50,7 @@ app.use('/*', (req, res) => {
   res.status(404).send({ message: 'Страница не найдена' });
 });
 
-// app.use(errors());
+app.use(errors());
 
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,7 @@ router.get('/users/me', getUserMe);
 
 router.get('/users/:userId', celebrate({
   params: Joi.object().keys({
-    userId: Joi.string().required().length(24).hex(),
+    _id: Joi.string().required().length(24).hex(),
   }),
 }), getUserById);
 
